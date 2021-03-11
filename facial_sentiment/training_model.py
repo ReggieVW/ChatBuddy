2896209d21d26a11a776bb27da30def8399d735b
ReggieVW <reggievanwoensel@yahoo.com>
2021-03-11 18:34:11 +0100

    Changed directory

--- facial_sentiment/training_model.py
+++ facial_sentiment/training_model.py
@@ -8,40 +8,40 @@ import os
 from keras.optimizers import RMSprop,SGD,Adam
 from keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
 
-num_classes = 7
+num_classes = 8
 img_rows,img_cols = 48,48
 batch_size = 32
 
-train_data_dir = r'C:\Users\Shruti Sharma\PycharmProjects\datastats\images\train'
-validation_data_dir = r'C:\Users\Shruti Sharma\PycharmProjects\datastats\images\validation'
+train_data_dir = r'D:\Erasmus\dataset\archive\images\images\train'
+validation_data_dir = r'D:\Erasmus\dataset\archive\images\images\validation'
 
 train_datagen = ImageDataGenerator(
-					rescale=1./255,
-					rotation_range=30,
-					shear_range=0.3,
-					zoom_range=0.3,
-					width_shift_range=0.4,
-					height_shift_range=0.4,
-					horizontal_flip=True,
-					fill_mode='nearest')
+                    rescale=1./255,
+                    rotation_range=30,
+                    shear_range=0.3,
+                    zoom_range=0.3,
+                    width_shift_range=0.4,
+                    height_shift_range=0.4,
+                    horizontal_flip=True,
+                    fill_mode='nearest')
 
 validation_datagen = ImageDataGenerator(rescale=1./255)
 
 train_generator = train_datagen.flow_from_directory(
-					train_data_dir,
-					color_mode='grayscale',
-					target_size=(img_rows,img_cols),
-					batch_size=batch_size,
-					class_mode='categorical',
-					shuffle=True)
+                    train_data_dir,
+                    color_mode='grayscale',
+                    target_size=(img_rows,img_cols),
+                    batch_size=batch_size,
+                    class_mode='categorical',
+                    shuffle=True)
 
 validation_generator = validation_datagen.flow_from_directory(
-							validation_data_dir,
-							color_mode='grayscale',
-							target_size=(img_rows,img_cols),
-							batch_size=batch_size,
-							class_mode='categorical',
-							shuffle=True)
+                            validation_data_dir,
+                            color_mode='grayscale',
+                            target_size=(img_rows,img_cols),
+                            batch_size=batch_size,
+                            class_mode='categorical',
+                            shuffle=True)
 
 
 model = Sequential()
@@ -150,3 +150,4 @@ history=model.fit_generator(
                 callbacks=callbacks,
                 validation_data=validation_generator,
                 validation_steps=nb_validation_samples//batch_size)
+model.save_weights("face_emotion.h5")
