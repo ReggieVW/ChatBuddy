61159a3abf883a2a96a1fa092f69ccb05f9eed07
ReggieVW <reggievanwoensel@yahoo.com>
2021-03-11 14:57:51 +0100

    Added facial sentiment analysis

--- video_capture.py
+++ video_capture.py
@@ -3,49 +3,52 @@ import cv2
 import io
 from keras.models import model_from_json
 from keras.preprocessing import image
+from keras.models import load_model
+from keras.preprocessing.image import img_to_array
 
 # initialize the Haar Cascade face detection model
 face_haar_cascade = cv2.CascadeClassifier(cv2.samples.findFile(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml'))
-#load model
-model = model_from_json(open("fer.json", "r").read())
 #load weights
-model.load_weights('fer.h5')
+model =load_model(r'facial_sentiment\Emotion_little_vgg.h5')
 
 video_capture = cv2.VideoCapture(0)
 
-while True:
-    ret,test_img=video_capture.read()# captures frame and returns boolean value and captured image
-    if not ret:
-        continue
-    gray_img= cv2.cvtColor(test_img, cv2.COLOR_BGR2GRAY)
+class_labels = ['Angry','Happy','Neutral','Sad','Surprise']
 
-    faces_detected = face_haar_cascade.detectMultiScale(gray_img, 1.32, 5)
+    while True:
+        ret,img=video_capture.read()# captures frame and returns boolean value and captured image
+        if not ret:
+            continue
+        gray= cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+        faces_detected = face_haar_cascade.detectMultiScale(gray, 1.32, 5)
 
-    for (x,y,w,h) in faces_detected:
-        cv2.rectangle(test_img,(x,y),(x+w,y+h),(255,0,0),thickness=7)
-        roi_gray=gray_img[y:y+w,x:x+h]#cropping region of interest i.e. face area from  image
-        roi_gray=cv2.resize(roi_gray,(48,48))
-        img_pixels = image.img_to_array(roi_gray)
-        img_pixels = np.expand_dims(img_pixels, axis = 0)
-        img_pixels /= 255
+        for (x,y,w,h) in faces_detected:
+            if cv2.waitKey(10) == ord('q'):#wait until 'q' key is pressed
+                print("pressed 'q'")
+                # When everything is done, release the capture
+                break
+            cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),thickness=7)
+            roi_gray = gray[y:y+h,x:x+w]
+            roi_gray = cv2.resize(roi_gray,(48,48),interpolation=cv2.INTER_AREA)
 
-        predictions = model.predict(img_pixels)
+            if np.sum([roi_gray])!=0:
+                roi = roi_gray.astype('float')/255.0
+                roi = img_to_array(roi)
+                roi = np.expand_dims(roi,axis=0)
 
-        #find max indexed array
-        max_index = np.argmax(predictions[0])
+            # make a prediction on the ROI, then lookup the class
+                preds = model.predict(roi)[0]
+                label=class_labels[preds.argmax()]
+                label_position = (x,y)
+                cv2.putText(img,label,label_position,cv2.FONT_HERSHEY_SIMPLEX,2,(0,255,0),3)
+            else:
+                cv2.putText(img,'No Face Found',(20,60),cv2.FONT_HERSHEY_SIMPLEX,2,(0,255,0),3)
 
-        emotions = ('angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral')
-        predicted_emotion = emotions[max_index]
+            resized_img = cv2.resize(img, (1000, 700))
 
-        cv2.putText(test_img, predicted_emotion, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
+            cv2.imshow('Facial emotion analysis ',resized_img)
 
-    resized_img = cv2.resize(test_img, (1000, 700))
-    cv2.imshow('Facial emotion analysis ',resized_img)
-
-
-    if cv2.waitKey(10) == ord('q'):#wait until 'q' key is pressed
-        break
 
 # When everything is done, release the capture
 video_capture.release()
