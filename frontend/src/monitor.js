251e4afe8abe6b01950c39d813fcd21d4b190b18
ReggieVW <reggievanwoensel@yahoo.com>
2021-04-02 23:06:02 +0200

    clean up code

--- frontend/src/monitor.js
+++ frontend/src/monitor.js
@@ -6,11 +6,8 @@ export class Monitor extends Component {
 	  super(props);
 	  this.state = {
 		items: [],
-		isLoaded: false,
-		test: 'reggie',
 		imageStatus: "loading"
 	  };
-	  
 	}
 
 	handleImageLoaded() {
@@ -21,33 +18,13 @@ export class Monitor extends Component {
 		this.setState({ imageStatus: "failed to load" });
 	  }
   
-	componentDidMount() {
-	  fetch('/test')
-		.then(res => res.json())
-		.then(result => {
-			console.log(result)
-		  this.setState({
-			isLoaded: true,
-			items: result,
-			test: result.name
-		  });
-		  this.state.test=result
-		  console.log(this.state.test)
-		});
-	}
-  
 	render() {
-	  const { items } = this.state.items;
-	  if (!this.state.isLoaded) {
-		return <div>Loading ... </div>;
-	  } else {
 		return (
 			<div>
 					<img src={'/video_feed'} className="App-logo" alt="logo" onLoad={this.handleImageLoaded.bind(this)} onError={this.handleImageErrored.bind(this)} />
 					{this.state.imageStatus}
    			</div>
 		);
-	  }
 	}
   }
 export default Monitor;
