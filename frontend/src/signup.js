436da2f7441a5fa0e235baad46d674fbccdba739
ReggieVW <reggievanwoensel@yahoo.com>
2021-04-29 10:47:38 +0200

    Changed br lines

--- frontend/src/signup.js
+++ frontend/src/signup.js
@@ -67,9 +67,10 @@ export class Signup extends Component {
 									<span className="login100-form-title p-b-53">
 										Sign Up
 									</span>
-									
 
+								
 									
+																
 									<div className="p-t-31 p-b-9">
 										<span className="txt1">
 											Username
@@ -79,11 +80,10 @@ export class Signup extends Component {
 										<input id="usernameid" className="input100" type="text" name="username" />
 										<span className="focus-input100"></span>
 									</div>
-
-
-									
 									<br/><br/>
 									<br/><br/>
+
+									<input/>
 									<br/><br/>
 									<br/><br/>
 									<br/><br/>
