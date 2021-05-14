5fe211ea9520bbcf63c3437b8df6c0c6d50ecf05
ReggieVW <reggievanwoensel@yahoo.com>
2021-05-14 15:08:46 +0200

    Added face recognition knn

---
+++ python_server/face_encoding/face_encoding_knn.py
@@ -0,0 +1,85 @@
+import math
+from sklearn import neighbors
+import os
+import os.path
+import pickle
+import face_recognition
+from face_recognition.face_recognition_cli import image_files_in_folder
+import csv
+
+
+def train(train_dir, model_save_path):
+    X = []
+    y = []
+    # Loop through each person in the training set
+    for class_dir in os.listdir(train_dir):
+        if not os.path.isdir(os.path.join(train_dir, class_dir)):
+            continue
+        # Loop through each training image for the current person
+        for img_path in image_files_in_folder(os.path.join(train_dir, class_dir)):
+            image = face_recognition.load_image_file(img_path)
+            face_bounding_boxes = face_recognition.face_locations(image)
+            if len(face_bounding_boxes) != 1:
+                # If there are no people (or too many people) in a training image, skip the image.
+                if verbose:
+                    print("Image {} not suitable for training: {}".format(img_path, "Didn't find a face" if len(face_bounding_boxes) < 1 else "Found more than one face"))
+            else:
+                # Add face encoding for current image to the training set
+                X.append(face_recognition.face_encodings(image, known_face_locations=face_bounding_boxes)[0])
+                y.append(class_dir)
+                
+    # Create and train the KNN classifier
+    knn_clf = neighbors.KNeighborsClassifier(n_neighbors=1, algorithm='ball_tree', weights='distance')
+    knn_clf.fit(X, y)
+
+    # Save the trained KNN classifier
+    with open(model_save_path, 'wb') as f:
+        pickle.dump(knn_clf, f)
+
+    return knn_clf
+
+
+def predict(X_img_path, model_path, distance_threshold=0.6):
+
+    with open(model_path, 'rb') as f:
+        knn_clf = pickle.load(f)
+
+    # Load image file and find face locations
+    X_img = face_recognition.load_image_file(X_img_path)
+    X_face_locations = face_recognition.face_locations(X_img)
+
+
+    # If no faces are found in the image, return the predicted face location 
+    if len(X_face_locations) == 0:
+        return []
+
+    # Find encodings for faces in the test iamge
+    faces_encodings = face_recognition.face_encodings(X_img, known_face_locations=X_face_locations)
+
+    # Use the KNN model to find the best matches for the test face
+    closest_distances = knn_clf.kneighbors(faces_encodings, n_neighbors=1)
+    are_matches = [closest_distances[0][i][0] <= distance_threshold for i in range(len(X_face_locations))]
+
+    # Predict classes and remove classifications that aren't within the threshold
+    return [(pred, loc) if rec else ("unknown", loc) for pred, loc, rec in zip(knn_clf.predict(faces_encodings), X_face_locations, are_matches)]
+
+
+if __name__ == "__main__":
+    # STEP 1: Train the KNN classifier and save it to disk
+    # Once the model is trained and saved, you can skip this step next time.
+    print("KNN Classifier is being trained")
+    classifier = train("images-train", model_save_path="trained_knn_model.clf")
+    print("Traing is done. You can use the trained model to predict ur test data now. ")
+
+    result = [['image_label', 'name']]
+    count = 0
+    # STEP 2: Using the trained classifier, make predictions for unknown images
+    for image_file in os.listdir("images-val-pub"):
+        print(image_file)
+        count +=1
+        if count % 100 == 0: print ("Processed {} images".format(count))
+        full_file_path = os.path.join("images-val-pub", image_file)
+        predictions = predict(full_file_path, "trained_knn_model.clf")
+        append_helper = [str(image_file), predictions[0][0]]
+        result.append(append_helper)
+print(result)
---
+++ python_server/face_encoding/face_recognition_testing.py
@@ -0,0 +1,104 @@
+import numpy as np
+import cv2
+import face_recognition
+import pickle
+
+def predict(frame, knn_clf=None, model_path=None, distance_threshold=0.6):
+    """
+    Recognizes faces in given image using a trained KNN classifier
+    :param knn_clf: (optional) a knn classifier object. if not specified, model_save_path must be specified.
+    :param model_path: (optional) path to a pickled knn classifier. if not specified, model_save_path must be knn_clf.
+    :param distance_threshold: (optional) distance threshold for face classification. the larger it is, the more chance
+           of mis-classifying an unknown person as a known one.
+    :return: a list of names and face locations for the recognized faces in the image: [(name, bounding box), ...].
+        For faces of unrecognized persons, the name 'unknown' will be returned.
+    """
+
+    if knn_clf is None and model_path is None:
+        raise Exception("Must supply knn classifier either thourgh knn_clf or model_path")
+
+    # Load a trained KNN model (if one was passed in)
+    if knn_clf is None:
+        with open(model_path, 'rb') as f:
+            knn_clf = pickle.load(f)
+
+    # find face locations from frame
+    X_face_locations = face_recognition.face_locations(frame)
+
+    # If no faces are found in the image, return an empty result.
+    if len(X_face_locations) == 0:
+        return []
+
+    # Find encodings for faces in the frame
+    faces_encodings = face_recognition.face_encodings(frame, known_face_locations=X_face_locations)
+
+    # Use the KNN model to find the best matches for the test face
+    closest_distances = knn_clf.kneighbors(faces_encodings, n_neighbors=1)
+    are_matches = [closest_distances[0][i][0] <= distance_threshold for i in range(len(X_face_locations))]
+
+    # Predict classes and remove classifications that aren't within the threshold
+    return [(pred, loc) if rec else ("unknown", loc) for pred, loc, rec in zip(knn_clf.predict(faces_encodings), X_face_locations, are_matches)]
+
+
+def show_labels_on_webcam(RGBFrame, predictions):
+    """
+    Shows the face recognition results visually.
+    :param img_path: path to image to be recognized
+    :param predictions: results of the predict function
+    :return:
+    """
+    frame = RGBFrame
+
+    for name, (top, right, bottom, left) in predictions:
+        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+        top *= 4
+        right *= 4
+        bottom *= 4
+        left *= 4
+
+        # Draw a box around the face
+        print (frame.shape)
+        print (frame.dtype)
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+        # Draw a label with a name below the face
+        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+
+    # Display the resulting image
+    cv2.imshow('Video', frame)
+
+# Get a reference to webcam #0 (the default one)
+video_capture = cv2.VideoCapture(0)
+
+while True:
+    # Grab a single frame of video
+    ret, frame = video_capture.read()
+    if cv2.waitKey(10) == ord('q'):#wait until 'q' key is pressed
+        print("pressed 'q'")
+        loop_break = False
+        # When everything is done, release the capture
+        break
+
+    # Resize frame of video to 1/4 size for faster face recognition processing
+    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+
+    # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+    rgb_small_frame = small_frame[:, :, ::-1]
+
+    predictions = predict(rgb_small_frame, model_path="trained_knn_model.clf")
+
+    # Display results overlaid on webcam video
+    print (rgb_small_frame.shape)
+    print (rgb_small_frame.dtype)
+    show_labels_on_webcam(frame, predictions)
+
+    # Hit 'q' on the keyboard to quit!
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release handle to the webcam
+video_capture.release()
+cv2.destroyAllWindows()
